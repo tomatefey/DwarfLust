47fa4d0ab801765c85367c70fc911021ce95cb1e
Ijoanet <joanramosm@gmail.com>
2017-02-05 20:38:30 +0100

    Starting ia enemy

--- Assets/Scripts/Enemy/Enemy_Behaviour.cs
+++ Assets/Scripts/Enemy/Enemy_Behaviour.cs
@@ -8,15 +8,25 @@ public class Enemy_Behaviour : MonoBehaviour {
     [Header ("Component")]
     NavMeshAgent agent;
     Transform target;
+    Animator anim;
 
 
     [Header("Movement")]
     public float minDist;
+    float forwardAmount;
+
+    bool isGrounded;
+    bool isMeleeAttack;
+
+    Vector3 move;
+    float animSpeedMultiplier;
+    
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -25,12 +35,35 @@ public class Enemy_Behaviour : MonoBehaviour {
 
         if(Vector3.Distance(this.transform.position, target.position) > minDist)
         {
+            forwardAmount = 1;
             agent.Resume();
         }
         else
         {
+            forwardAmount = 0;
             agent.Stop();
         }
         agent.destination = target.position;
+
+        UpdateAnimator();
 	}
+
+    void UpdateAnimator()
+    {
+        // update the animator parameters
+        anim.SetFloat("Forward", forwardAmount, 0.2f, Time.deltaTime);
+
+        anim.SetBool("OnGround", isGrounded);
+
+        anim.SetBool("isMeleeAttack", isMeleeAttack);
+
+        if (isGrounded && move.magnitude > 0)
+        {
+            anim.speed = animSpeedMultiplier;
+        }
+        else
+        {
+            anim.speed = 1;
+        }
+    }
 }
